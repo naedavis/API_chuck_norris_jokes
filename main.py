661f5fb6c3fa7f55d67fbf11f0647227db9b2f23
Naeemah Davis <82158710+naedavis@users.noreply.github.com>
2021-06-04 15:48:38 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import messagebox
+
 import requests
 import tkinter as tk
 from tkinter import *
@@ -7,26 +9,32 @@ root.geometry("300x300")
 root.config(bg="Black")
 
 
-
 def get_chuck_jokes():
-    response = requests.get("https://api.chucknorris.io/jokes/random")
-    data = response.json()
-    return data["value"]
+    try:
+        response = requests.get("https://api.chucknorris.io/jokes/random")
+        data = response.json()
+        return data["value"]
+    except requests.exceptions.ConnectionError as x:
+        messagebox.showinfo("error", "Connection Error")
+
 
 
 msg_joke = Message(root, text=get_chuck_jokes(),bg="black", fg="white", font="Arial 20")
 msg_joke.pack()
 
 
-def next():
-    response = requests.get("https://api.chucknorris.io/jokes/random")
-    data = response.json()
-    joke = data["value"]
-    msg_joke.config(text=get_chuck_jokes())
+def next_joke():
+    try:
+        response = requests.get("https://api.chucknorris.io/jokes/random")
+        data = response.json()
+        joke = data["value"]
+        msg_joke.config(text=get_chuck_jokes())
+    except requests.exceptions.ConnectionError as x:
+        messagebox.showinfo("error", "Connection Error")
 
-btn_next = Button(root, text="NEXT JOKE", fg="pink", command=next)
+
+btn_next = Button(root, text="NEXT JOKE", fg="pink", command=next_joke)
 btn_next.pack()
-
 
 
 root.mainloop()
